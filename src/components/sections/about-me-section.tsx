bb365dad4a607bcd41e405e742b33cc9e95738fd
Sujal Kumar <www.sujalmuz1245@gmail.com>
2025-07-21 11:02:02 +0000

    Use public/images/aboutme.jpeg

--- src/components/sections/about-me-section.tsx
+++ src/components/sections/about-me-section.tsx
@@ -17,7 +17,7 @@ export default function AboutMeSection() {
           </div>
           <div className="flex items-center justify-center">
              <Image
-              src="https://placehold.co/550x310.png"
+              src="/images/aboutme.jpeg"
               alt="About me image"
               width={550}
               height={310}
