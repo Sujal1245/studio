166a86e6f29eba34861eb556000f653bddb645bb
Sujal Kumar <www.sujalmuz1245@gmail.com>
2025-07-21 10:28:01 +0000

    Any way to get Jetpack or Compose icon?

--- src/components/sections/hero-section.tsx
+++ src/components/sections/hero-section.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
-import { MoveRight, CodeXml } from "lucide-react";
+import { MoveRight } from "lucide-react";
 import AnimatedSection from "@/components/animated-section";
 
 export default function HeroSection() {
@@ -12,7 +12,14 @@ export default function HeroSection() {
           <div className="flex flex-col justify-center space-y-6">
             <div className="space-y-2 text-center lg:text-left">
               <div className="mb-4 inline-block animate-float">
-                <CodeXml className="h-24 w-24 text-primary drop-shadow-[0_2px_4px_rgba(var(--primary-rgb),0.4)]" />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  className="h-24 w-24 text-primary drop-shadow-[0_2px_4px_rgba(var(--primary-rgb),0.4)]"
+                  fill="currentColor"
+                >
+                  <path d="M12.3,4.95,12,4.8l-.3.15L5.85,9.2,5.56,9.44l.21,1.19.21,1.19.49-.24,5.4-2.7.1-.05.1.05,5.4,2.7.49.24.21-1.19.21-1.19-.29-.24Zm-6.72,9L5.4,13.7v2.38l.18.09,6,3,.42.21.42-.21,6-3,.18-.09V13.7l-.18-.09-5.4-2.7-.3-.15-.3.15-5.4,2.7Z" />
+                </svg>
               </div>
               <h1 className="font-headline text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl lg:text-7xl text-primary">
                 Sujal Kumar
